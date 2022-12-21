b3c3e71f499646074af6a66d41ef6bf435471c0d
JORIS DEPOORTERE <joris.depoortere@outlook.com>
2022-12-21 20:10:19 +0100

    All outside scripts now added as methods

--- SolNet.py
+++ SolNet.py
@@ -1,38 +1,183 @@
 # Do we need to reference PVLIB somehow when we use this? Or make our own version of the API connection?
 from darts.models import BlockRNNModel
-from DataGathering import SourceData as Source
-from DataTransforming import RawData
+import numpy as np
+import pandas as pd
 
+from darts import TimeSeries
+from darts.utils.timeseries_generation import datetime_attribute_timeseries
+from darts.dataprocessing.transformers import Scaler
 from pytorch_lightning.callbacks import EarlyStopping
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+
+import pvlib_helpers
+
 class SolNet():
-        
-    def data(
-        latitude, 
-        longitude, 
-        peakPower,
-        locations,
-        ):
+
+            
+    """
+    Overhaul of the product is happening:
+    - Everything will be centered in one class
+    - Solnet will be initialized with a latitude and longitude as args and the rest will be kwargs
+    - The initialisation will bring forth a data series
+    - Everything else will be done with methods:
+        - Data prep
+        - Model creation
+        - Model evaluation (This should work both out of the box on the source locations AND on the target location if the user calls it)    
+    """
     
-        print('Fetching Source Model data\n')
-        
-        source_data = Source.dataGathering(latitude, longitude, peakPower, locations, start_date = start_date)
+    #Class attributes
+    km_radius = 50          #The radius around the actual location to find additional locations
+    gaus_radius = 0.5       #The covariance for gaussian noise in km on the radius
+    precision = 40          #The closeness to trailing the coastline when locations are close to water
 
-        return source_data
+    def dataGathering(latitude, longitude, peakPower, locations, start_date = 2005):
+        
+        data = []
+        
+        # Make a dataframe for the locations
+        lin_loc = np.linspace(0,locations,locations)
+        lin_loc_df = pd.DataFrame({"Locations":lin_loc})
+
+        # Make a sin and cosin pattern based on the number of locations
+        lin_loc_df["loc_norm"] = 2 * math.pi * lin_loc_df["Locations"] / lin_loc_df["Locations"].max()
+
+        lin_loc_df["sin_loc"] = np.sin(lin_loc_df["loc_norm"])
+        lin_loc_df["cos_loc"] = np.cos(lin_loc_df["loc_norm"])
+
+        # Base location
+
+        print('Gathering data from base location...')
+        
+        try:
+            data.append(pvlib_helpers.get_pvgis_hourly(latitude=latitude,
+                                          longitude=longitude, 
+                                          start = start_date,
+                                          pvcalculation = True,
+                                          peakpower=peakPower,
+                                          optimal_surface_tilt=True, 
+                                          optimalangles=True))
 
 
-    def dataprep(
-        source_data
-        ):
+            if sum(data[-1][0]['temp_air'])==0:
+                raise ValueError("Location has no weather data, trying different location" + "...\n")  
+                   
+        except ValueError as ve:
+            print(ve) 
+    
+        except:
+            print('Location over sea, please provide coordinates on land')
 
-        print('Data gathered\n')
+        # Additional locations
+
+        for i in range(locations - 1):
+            
+            print('Gathering data from additional location ' + str(i+1) + '...\n')
+            
+            # The distance from the base location, transforming latitude and longitude to kilometers
+            lat_dif = (1/110.574) * SourceData.km_radius
+            lat = latitude + lin_loc_df['sin_loc'][i]*lat_dif
+            
+            # Longitude is based on the actual latitude 
+            long_dif = 1/(111.32*abs(math.cos(math.radians(lat)))) * SourceData.km_radius
+            long = longitude + lin_loc_df['cos_loc'][i]*long_dif
+            
+            # Gaussian randomisation
+            lat_dif_gaus = (1/110.574) * SourceData.gaus_radius
+            long_dif_gaus = 1/(111.32*abs(math.cos(math.radians(lat)))) * SourceData.gaus_radius
+            
+            mean = [long, lat]
+            cov = [[long_dif_gaus,0],
+                   [0,lat_dif_gaus]]
+            
+            x, y = np.random.multivariate_normal(mean, cov, 1).T
+            long = x[0]
+            lat = y[0]
+            
+            # Check if location is on land 
+            ## If yes: append to the list
+            
+            long_list = np.linspace(longitude, long, SourceData.precision)
+            lat_list = np.linspace(latitude, lat, SourceData.precision)
+                
+            for i in range(0,(SourceData.precision-1)):
+                try: 
+                    long_new = long_list[-(i+1)]
+                    lat_new = lat_list[-(i+1)]
+                    data.append(pvlib_helpers.get_pvgis_hourly(latitude=lat_new,
+                                                longitude=long_new, 
+                                                start = start_date,
+                                                pvcalculation = True,
+                                                peakpower=peakPower,
+                                                optimal_surface_tilt=True, 
+                                                optimalangles=True))
+                        
+                    if sum(data[-1][0]['temp_air'])==0:
+                        del(data[-1])
+                        raise ValueError("Location has no weather data, trying different location" + "...\n")  
+                    else:
+                        break
+                            
+                except ValueError as ve:
+                    print(ve)
+                        
+                except:
+                    print('Location over sea, trying different location' + '...\n')
         
-        print('Transforming data: Removing unused variables, scaling, featurisation \n')
+        return data
+    
+    def dataTransforming(
+        dataList,
+        covariates = ['poa_direct','poa_sky_diffuse','poa_ground_diffuse','solar_elevation', 'temp_air']):
         
-        trainList, covTrainList, testList, covTestList = RawData.dataTransforming(source_data)        
+        trainList = []
+        covTrainList = []
+        testList = []
+        covTestList = []
+              
+        TRAINTESTSPLIT = 0.85
         
+        for i in range(len(dataList)):
+            
+            source_data = dataList[i][0].astype(np.float32)
+        
+            #Create a TimeSeries object of the target variable
+            target_series = TimeSeries.from_series(source_data['P'])
+        
+            # Create training and validation sets of the target variable
+            train, test = target_series.split_after(TRAINTESTSPLIT)
+            transformer = Scaler()
+            train = transformer.fit_transform(train)
+            test = transformer.transform(test)     
+
+            #Create a TimeSeries object of the target variable
+            covariate_series = TimeSeries.from_series(source_data[covariates])
+
+            hour_series = datetime_attribute_timeseries(
+                pd.date_range(start=covariate_series.start_time(), freq=covariate_series.freq_str, periods=len(source_data)),
+                attribute="hour",
+                cyclic=True
+                ).astype(np.float32)
+
+            covariate_series = covariate_series.stack(hour_series)
+
+
+            # Create training and validation sets of the target variable
+            cov_train, cov_test = covariate_series.split_after(TRAINTESTSPLIT)
+            transformer_2 = Scaler()
+            cov_train = transformer_2.fit_transform(cov_train)
+            cov_test = transformer_2.transform(cov_test)
+
+            trainList.append(train)
+            covTrainList.append(cov_train)
+            testList.append(test)
+            covTestList.append(cov_test)
+
         return trainList, covTrainList, testList, covTestList
-    
     
     def model(
         trainList, 
@@ -95,3 +240,6 @@ class SolNet():
         my_model = BlockRNNModel.load_from_checkpoint(modelname, best=True)
     
         return my_model
+     
+    def modelEvaluation():
+        return None
