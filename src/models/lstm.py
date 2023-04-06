c8e670c667cb291fb74890d7824092d006ff85c7
Joris-Dprtr <joris.depoortere@kuleuven.be>
2023-04-06 12:12:50 +0200

    Models and training

---
+++ src/models/lstm.py
@@ -0,0 +1,30 @@
+from torch import nn
+
+class LSTM(nn.Module):
+    def __init__(
+        self, 
+        input_size,
+        hidden_size, 
+        num_layers,
+        output_size,
+        dropout):
+        super().__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.output_size = output_size
+        self.dropout = dropout
+                
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, dropout=dropout, batch_first = True)
+        self.linear = nn.Linear(in_features=hidden_size, out_features=output_size)
+
+    def forward(self, input):
+        hidden, _ = self.lstm(input, None)
+        if hidden.dim() == 2:
+            hidden = hidden[-1,:]
+        else:
+            hidden = hidden[:,-1,:]
+        output = self.linear(hidden)
+        
+        return output
