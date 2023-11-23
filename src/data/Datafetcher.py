b0008c424b7fa355f5a56877471c6847699b8e8b
Joris-Dprtr <joris.depoortere@kuleuven.be>
2023-11-23 15:02:14 +0100

    Docstrings added

--- src/data/Datafetcher.py
+++ src/data/Datafetcher.py
@@ -7,42 +7,51 @@ from ..util import pvgis_api as pvgis
 
 class DataFetcher:
 
-    """
-    A class that gathers data from a location (and several neighbouring locations) on PVGIS, using the PVGIS API
-    utility (see util).
-
-    Methods:
-    - init: set all the key parameters required for PVGIS and gather the data by executing _data_gathering
-    - _data_gathering: gather data from PVGIS based on the parameters given in the init
-    - save_data: save the data in a pickle file
-    """
-
     def __init__(
-        self,
-        latitude,
-        longitude,
-        peakPower,
-        tilt,
-        azimuth,
-        locations=5,
-        start_date=2005,
-        km_radius=50,                 # The radius around the actual location to find additional locations
-        gaus_radius=0.5,              # The covariance for gaussian noise in km on the radius
-        precision=40):
-
+            self,
+            latitude,
+            longitude,
+            peak_power,
+            tilt,
+            azimuth,
+            start_date=2005,
+            locations=5,
+            km_radius=50,  # The radius around the actual location to find additional locations
+            gaus_radius=0.5,  # The covariance for gaussian noise in km on the radius
+            precision=40):
+        """
+        A class that gathers data from a location (and several neighbouring locations) on PVGIS, using the PVGIS API
+        utility (see util).
+        :param latitude: latitude of the location of interest
+        :param longitude: longitude of the location of interest
+        :param peak_power: the peakPower of the installation
+        :param tilt: the tilt of the solar panels
+        :param azimuth: the direction of the solar panels
+        :param start_date: the start date to gather data (minimum = 2005)
+        :param locations: total locations for which we gather data. 5 locations means the base location + 4 others
+        :param km_radius: the radius around the original location to find additional locations
+        :param gaus_radius: a gaussian noise factor to include in the km_radius
+        :param precision: the precision for when a location is located close to the sea
+        """
         self.km_radius = km_radius
         self.gaus_radius = gaus_radius
         self.precision = precision
 
-        self.pv = pvgis.PVgis(latitude, longitude, start_date, tilt, azimuth, peakPower)
+        self.pv = pvgis.PVgis(latitude, longitude, start_date, tilt, azimuth, peak_power)
 
         self.dataset = self._data_gathering(latitude, longitude, locations=locations)
 
     def _data_gathering(self, latitude, longitude, locations=5):
-
+        """
+        Gather data for a specific location and (if locations > 1) nearby locations
+        :param latitude: latitude of the location of interest
+        :param longitude: longitude of the location of interest
+        :param locations: total locations for which we gather data. 5 locations means the base location + 4 others
+        :return: list of length locations with PV + weather data for each of them
+        """
         data = []
 
-        additional_locations = fm.circle_pattern(locations-1)
+        additional_locations = fm.circle_pattern(locations - 1)
 
         # Base location
 
@@ -70,20 +79,20 @@ class DataFetcher:
 
         for i in range(locations - 1):
 
-            print(f'Gathering data from additional location {i+1}...')
+            print(f'Gathering data from additional location {i + 1}...')
 
             # The distance from the base location, transforming latitude and longitude to kilometers
-            lat_additional = latitude + additional_locations['Sine'][i]*lat_dif
+            lat_additional = latitude + additional_locations['Sine'][i] * lat_dif
 
             # Longitude is based on the actual latitude and has to be calculated in the loop
             long_dif = fm.km_to_long(self.km_radius, lat_additional)
-            long_additional = longitude + additional_locations['Cosine'][i]*long_dif
+            long_additional = longitude + additional_locations['Cosine'][i] * long_dif
 
             # Gaussian randomisation longitude (has to be calculated in the loop) 
-            long_dif_gaus = fm.km_to_long(self.gaus_radius,lat_additional)
+            long_dif_gaus = fm.km_to_long(self.gaus_radius, lat_additional)
 
             mean = [long_additional, lat_additional]
-            cov = [[long_dif_gaus,0], [0,lat_dif_gaus]]
+            cov = [[long_dif_gaus, 0], [0, lat_dif_gaus]]
 
             x, y = np.random.multivariate_normal(mean, cov, 1).T
             long_additional, lat_additional = x[0], y[0]
@@ -97,14 +106,14 @@ class DataFetcher:
             long_list = long_arr.tolist()
             lat_list = lat_arr.tolist()
 
-            for i in range(0,(self.precision-1)):
+            for i in range(0, (self.precision - 1)):
                 try:
-                    long_new = long_list[-(i+1)]
-                    lat_new = lat_list[-(i+1)]
+                    long_new = long_list[-(i + 1)]
+                    lat_new = lat_list[-(i + 1)]
                     data.append(self.pv.get_pvgis_hourly())
 
                     if sum(data[-1]['T2m']) == 0:
-                        del(data[-1])
+                        del (data[-1])
                         raise ValueError("Location has no weather data, trying different location" + "...")
                     else:
                         break
@@ -124,7 +133,11 @@ class DataFetcher:
         return pv_dataset_list
 
     def save_data(self, file_name=None):
-
+        """
+        Save the data in a pickle file
+        :param file_name: give a name to the file (optional, if None, a datetime string is given)
+        :return: returns the file name
+        """
         now = datetime.datetime.now()
         date_string = now.strftime("%y%m%d_%H%M")
         if file_name is None:
--- src/data/Featurisation.py
+++ src/data/Featurisation.py
@@ -3,7 +3,11 @@ import pickle
 
 
 def _load_data(file_path):
-
+    """
+    load data from a given file path
+    :param file_path: the file path name
+    :return: return the data
+    """
     try:
         with open(file_path, 'rb') as f:
             data = pickle.load(f)
@@ -13,9 +17,13 @@ def _load_data(file_path):
     return data
 
 
-class Featurisation():
+class Featurisation:
 
     def __init__(self, data):
+        """
+        Include features for the training of the models
+        :param data: the original dataframe (in list format as provided by datafetcher.py or as a file name)
+        """
         if data is None:
             raise ValueError("Data cannot be None. Please provide a (list of) pandas dataframe(s) or a file path.")
         elif isinstance(data, list):
@@ -26,16 +34,25 @@ class Featurisation():
             raise ValueError("Invalid data type provided. Must be a (list of) pandas dataframe(s) or a file path.")
 
     def base_features(self, featurelist):
-
+        """
+        Features to include that are already provided by PVGIS
+        :param featurelist: list of features that we want to include in the model
+        :return: the data list but with only the features provided in the featurelist included
+        """
         for i in range(len(self.data)):
             self.data[i] = self.data[i][featurelist]
 
         return self.data
 
-    def cyclic_features(self, yearly=True, hourly=True):
-
+    def cyclic_features(self, yearly=True, daily=True):
+        """
+        Cyclical features to include in the model
+        :param yearly: yearly cyclical features, transforming the months of the year in sin and cos features
+        :param daily: daily cyclical features, transforming the hours of the day in sin and cos features
+        :return: the data list but with chosen cyclic features included
+        """
         for i in range(len(self.data)):
-            if hourly is True:
+            if daily is True:
                 self.data[i]['hour_sin'] = np.sin(2 * np.pi * self.data[i].index.hour / 24)
                 self.data[i]['hour_cos'] = np.cos(2 * np.pi * self.data[i].index.hour / 24)
             if yearly is True:
